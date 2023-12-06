1e441656f0163bada3959185c9081f5d639fa08b
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-06 18:59:36 +0530

    Add files via upload

---
+++ basics01/01variable.js
@@ -0,0 +1,11 @@
+//console.log("sourab")
+const accountId=101;
+let accountEmail="sou@gmail.com";
+var accountPassword="2121";
+accountCity="hyd"
+//accountId=22;    error can not change value const
+//console.log(accountId);
+//console.log(accountEmail);
+//console.log(accountPassword);
+accountEmail="rab@gmail.ccom"
+console.table([accountEmail,accountId,accountPassword,accountCity])
