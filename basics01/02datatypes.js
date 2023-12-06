99c49c8e3b8b69a208cc3b3a14d3b886d766312e
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-06 19:00:29 +0530

    Add files via upload

---
+++ basics01/02datatypes.js
@@ -0,0 +1,40 @@
+/* console.log("sourab")  //sourab
+console.log(2+1)          //3  
+console.log("2+1")  //2+1
+console.log("2"+1)      //21
+console.log(2+"1")      //21
+console.log(2+1)        //3
+console.log("2+1"+5)    2+15
+console.log(6+"2+1")        //62+1
+console.log("6+2+1")  6+2+1
+ */
+
+"use strict"; //treat all js code as newer version
+/* alert(3+3) */ // we are using nodejs, not browser
+//alert is a function of window object in browser but it's undefined in nodejs
+
+console.log(3+
+     
+    3);// code readbility should be high not write in this manner
+
+let name="sourab"
+let age=18;
+let isLoggedIn=false
+let city=null
+let state;
+
+//number => 2 to power 53
+//bigint (used in trading sites)
+//string=> ""
+//boolean => true or false
+//null =>standalone value
+//undefined =>
+//symbol => for uniqueness we use
+
+//object 
+
+console.log(typeof "sourab"); //string
+console.log(typeof age);  //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
