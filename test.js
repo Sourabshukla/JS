425512c8517fc1f5188356e6cde69e89918a5f17
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-06 10:04:04 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,9 @@ let accountEmail="sou@gmail.com";
 var accountPassword="2121";
 accountCity="hyd"
 //accountId=22;    error can not change value const
+/*
+prefer not use var because issue in block scope and functional scope
+*/
 //console.log(accountId);
 //console.log(accountEmail);
 //console.log(accountPassword);
