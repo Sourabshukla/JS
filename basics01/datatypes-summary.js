a63b4b6f86d87834caddf5be95f87c3702199dd7
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-06 22:47:10 +0530

    Add files via upload

---
+++ basics01/datatypes-summary.js
@@ -0,0 +1,34 @@
+/* const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail;//undefined
+
+const id=Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id===anotherId);
+const bigNumber=356464648975464n
+
+console.log(typeof bigNumber);
+
+
+ */
+
+const heros=["shaktiman","nagaraj","doga"]
+let myobj={
+    name: "sourab",
+    age: 22 
+
+}
+
+const myFunction =function(){
+    console.log("hello world");
+}
+
+const outsideTemp=null
+console.log(typeof outsideTemp ); //object
+console.log(myFunction); //function
+
+
