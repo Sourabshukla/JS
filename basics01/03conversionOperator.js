4771cac35e8dfb4ac2421d23e631ca03cd99c47e
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-06 21:31:58 +0530

    Add files via upload

---
+++ basics01/03conversionOperator.js
@@ -0,0 +1,106 @@
+/* let score=33;
+console.log(typeof score)
+//number
+let s="33"
+console.log(typeof s);
+//string
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);  //number
+console.log(valueInNumber);//33
+ */
+/* let sc="33abc"
+console.log(typeof sc);//string
+let valueInNumber=Number(sc) //
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber);//NaN */
+
+/* let score=null
+let valueInNumber=Number(score)
+console.log(valueInNumber); //0
+ */
+
+/* let score=undefined
+let valueInNumber=Number(score)
+console.log(valueInNumber); //NaN */
+
+/* let score=true
+
+let valueInNumber=Number(score)
+console.log(valueInNumber); //1 */
+
+/* let sc=false
+let valueInNumber=Number(sc)
+console.log(valueInNumber);//0 */
+
+
+/* let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); */ // true
+
+/* let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);  //false */
+
+/* let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);  //33
+console.log(typeof stringNumber); //string */
+
+
+
+
+
+// ******** OPEERATIONS*******************
+
+/* let value=3
+let negValue=-value
+console.log(negValue);    //-3 */
+
+/* console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*2); //4
+console.log(2**3); //8 (2*2*2)
+console.log(2/3); //(0.66)
+console.log(10%3);  //1 */
+
+/* let str1="hello"
+let str2=" sourab"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);//12
+
+console.log(1+"2");  //12
+console.log("1+2"); //1+2
+
+console.log("1"+"2"); //12
+
+console.log("1"+2+2);  //122
+console.log(1+2+"2"); //32
+console.log(3+4*5%3); //5
+console.log((3+4)*5%3); */
+  //note
+  /*
+        If string is in first then all treated as string 
+        if string is in last Conversion will happen before numbers
+  */
+
+   /*  console.log(+true);  //1
+    console.log(-false); //-0
+    console.log(+"");//0
+    console.log(-""); //-0 */
+    /* let num1,num2,num3
+    num1=num=2=num3 =2+2 */
+
+
+   /* let gameCounter = 100
+   gameCounter++;
+   console.log(gameCounter); */ //101
+
+   let gameCounter = 100
+   ++gameCounter;
+   console.log(gameCounter);
+   console.log(gameCounter)
