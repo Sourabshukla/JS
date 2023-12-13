1ac3e657c6e2cea778f6f6cdc4d688714082ad8e
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-13 23:01:33 +0530

    Add files via upload
    Immediately Invoked function Expression (IIFE)

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,31 @@
+//iife => Immediately Invoked function Expression (IIFE)
+//Parentheses are used to contain the JavaScript code, and the term "immediate" refers to the fact that this pattern of wrapping JavaScript code
+
+//  function chai(){
+//     console.log(`DB CONNECTED`);
+//  }
+//  chai()
+
+(function chai(){
+   //named IIFE
+   console.log(`DB CONNECTED`);
+})();
+
+// 2 IIFE likhte hai jab tab 1st vale ke baad ; lagna compulsory
+
+//()()  //1st function defination 2nd execution call
+
+ // global scope ke pollution se priblem hoti hai kai baar so global scope ke jo variable hai usko hatane ke liye iife ko use krte hai
+
+ // 1st code ko khatam hone ke baad ; lagana
+
+ ((name)=>{
+   //unnamed IIFE
+   console.log(`DB Connect two ${name}`);
+ })('sourab')  // direct gave parameter we can call chai('')
+   
+
+
+// (function aurcode(){
+//    console.log(`DB Connect two`);
+//  })
