9aee7fef27e4c1a9af8c0315e78474124f4cce06
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-15 18:55:30 +0530

    Add files via upload

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,49 @@
+//syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("Feburary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
+const month="march"
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("Feburary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
