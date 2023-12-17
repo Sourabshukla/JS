7116185253c152f1782710d69097714af100ab78
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-18 00:58:47 +0530

    Add files via upload

---
+++ 05_iterations/two.js
@@ -0,0 +1,40 @@
+//while loop
+
+//syntax
+
+// while (condition) {
+    
+// }
+
+let i=0
+while (i<=10) {
+   // console.log(`value of index is ${i}`);
+    i=i+2
+}
+//console.log("outside the loop");
+
+myArray=['flash', 'batman', 'superman']
+let arr=0
+while (arr < myArray.length) {
+    //console.log(`value is ${myArray[arr]}`);
+    arr++
+}
+
+// do while loop
+ //Synatax
+// do {
+    
+// } while (condition);
+
+// let score=1
+// do {
+//    // console.log(`score is ${score}`);
+//     score++
+// } while (score<=10);
+
+
+let score=12
+do {
+   console.log(`score is ${score}`);// only once the loop runs
+    score++
+} while (score<=10);
