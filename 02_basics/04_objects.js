cde1d08faac52417f779273373e8b035aec89c03
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-26 21:28:01 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -23,12 +23,16 @@ const regularUser={
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 
-//const obj3={obj1,obj2}
-//const obj3=Object.assign(obj1,obj2)  //objects=>Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+// const obj3={obj1,obj2}
+//  const obj3=Object.assign(obj1,obj2)  
+
+
+//objects=>Copy the values of all of the enumerable own properties from one or more source 
+//objects to a target object. Returns the target object.
  
 const obj3={...obj1, ...obj2}  //spread operator
 
-//console.log(obj3);
+// console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 const users=[
     {
@@ -51,13 +55,13 @@ const users=[
 
 ]
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));//Returns an array of key/values of the enumerable properties of an object
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));// true    Determines whether an object has a property with the specified name.
-console.log(tinderUser.hasOwnProperty("isLoggedof"));//false
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));//Returns an array of key/values of the enumerable properties of an object  (or)  gives the id value seperate for all
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));// true    Determines whether an object has a property with the specified name.
+// console.log(tinderUser.hasOwnProperty("isLoggedof"));//false
 
 
 
@@ -66,19 +70,21 @@ const course={
     price:999,
     courseInstructor:"hitesh"
 }
-//course.courseInstructor    // here for accesing this we have to write so manyb times so we use below step
+course.courseInstructor    // here for accesing this we have to write so manyb times so we use below step
 
-//const{courseInstructor}=course
-//console.log(courseInstructor);
+const{courseInstructor}=course
+// console.log(courseInstructor);
 
 //destructure of object if name is long so we can use small name like below
 const {courseInstructor: instructor} = course
-//console.log(instructor);
+console.log(instructor);
 
 // whenever when we see any in  {}   so we need to think its destructured
+
 
 //json format
 /* {
     "name":"sourab",
     "age":23
 } */
+
