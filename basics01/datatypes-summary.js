d251c1090469a1148d72aa2fca246c80ebcf5332
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-26 19:13:30 +0530

    Update datatypes-summary.js

--- basics01/datatypes-summary.js
+++ basics01/datatypes-summary.js
@@ -1,22 +1,21 @@
-/* const score=100
-const scoreValue=100.3
-const isLoggedIn=false
-const outsideTemp=null
+// const score=100
+// const scoreValue=100.3
+// const isLoggedIn=false
+// const outsideTemp=null
 
-let userEmail;//undefined
+// let userEmail;//undefined
 
-const id=Symbol('123')
-const anotherId =Symbol('123')
+//  const id=Symbol('123')// symbol=>Returns a new unique Symbol value.
 
-console.log(id===anotherId);
-const bigNumber=356464648975464n
+//  const anotherId =Symbol('123')
 
-console.log(typeof bigNumber);
+// console.log(id===anotherId); //false
+// const bigNumber=356464648975464n
+
+// console.log(typeof bigNumber); //bigint
 
 
- */
-
-const heros=["shaktiman","nagaraj","doga"]
+/* const heros=["shaktiman","nagaraj","doga"]
 let myobj={
     name: "sourab",
     age: 22 
@@ -27,35 +26,13 @@ const myFunction =function(){
     console.log("hello world");
 }
 
- Premitive Datatypes
-
-                  Type                                   typeof
-
-i)             Number                               number
-ii)            String                                   string
-iii)           Boolean                               boolean
-iv)           Bigint                                   bigint
-v)            Symbol                                symbol
-vi)           Null                                      object
-vii)          Undefined                           undefined
-
-
-* Non-Premitive OR Referance OR Object datatype
-
-                  Type                                   typeof
-
-i)               Object                                object
-ii)              Array                                  object
-iii)             Function                            function(object)
 const outsideTemp=null
 console.log(typeof outsideTemp ); //object
 console.log(myFunction); //function
-
-
+ */
 
 //  stack (primitive)    , Heap(non-premitive or reference)
-// stack=> will get copy
-//Heap=> will get reference means whatever we change it changes the original value 
+
 
 let name="hello"
 let anotherName=name;
@@ -72,3 +49,5 @@ let userTwo=user
 userTwo.email="sourab@google.com"
 console.log(user.email);    //sourab@google.com
 console.log(userTwo.email);//sourab@google.com 
+// Primitives are passed by Value, Objects are Passed by Reference
+// undefined and null => primitive data types
