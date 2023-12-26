1ffb4a6737c8367c1f7b5d9c563ca55c3b6cbe4a
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-26 13:41:46 +0530

    Update 03conversionOperator.js

--- basics01/03conversionOperator.js
+++ basics01/03conversionOperator.js
@@ -1,106 +1,121 @@
-/* let score=33;
-console.log(typeof score)
-//number
+//let score=33;
+// console.log(typeof score)          //number
+
 let s="33"
-console.log(typeof s);
-//string
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);  //number
-console.log(valueInNumber);//33
- */
-/* let sc="33abc"
-console.log(typeof sc);//string
-let valueInNumber=Number(sc) //
-console.log(typeof valueInNumber); //number
-console.log(valueInNumber);//NaN */
+//console.log(typeof s);             //string
 
-/* let score=null
-let valueInNumber=Number(score)
-console.log(valueInNumber); //0
- */
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);//33
 
-/* let score=undefined
-let valueInNumber=Number(score)
-console.log(valueInNumber); //NaN */
+// let sc="33abc"
+// console.log(typeof sc);//string
+// let valueInNumber=Number(sc) //
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber);//NaN
+
+// let score=null
+// let valueInNumber=Number(score)
+// console.log(valueInNumber); //0
+
+
+// let score=undefined
+// let valueInNumber=Number(score)
+// console.log(valueInNumber); //NaN
 
 /* let score=true
 
 let valueInNumber=Number(score)
 console.log(valueInNumber); //1 */
 
-/* let sc=false
-let valueInNumber=Number(sc)
-console.log(valueInNumber);//0 */
+// let sc=false
+// let valueInNumber=Number(sc)
+// console.log(valueInNumber);//0
 
 
-/* let isLoggedIn=1
+// let isLoggedIn=1
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); */ // true
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
 
-/* let isLoggedIn=""
+// let isLoggedIn=""
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);  //false */
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);  //false
 
-/* let someNumber=33
+let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);  //33
-console.log(typeof stringNumber); //string */
-
+// console.log(stringNumber);  //33
+// console.log(typeof stringNumber); //string
 
 
 
 
 // ******** OPEERATIONS*******************
 
-/* let value=3
-let negValue=-value
-console.log(negValue);    //-3 */
+// let value=3
+// let negValue=-value
+// console.log(negValue);    //-3
 
-/* console.log(2+2); //4
-console.log(2-2); //0
-console.log(2*2); //4
-console.log(2**3); //8 (2*2*2)
-console.log(2/3); //(0.66)
-console.log(10%3);  //1 */
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2**3); //8 (2*2*2)
+// console.log(2/3); //(0.66)
+// console.log(10%3);  //1
 
-/* let str1="hello"
-let str2=" sourab"
+// let str1="hello"
+// let str2=" sourab"
 
-let str3= str1+str2
-console.log(str3);
+// let str3= str1+str2
+// console.log(str3);// hello sourab
 
-console.log("1"+2);//12
+// console.log("1"+2);//12
 
-console.log(1+"2");  //12
-console.log("1+2"); //1+2
+// console.log(1+"2");  //12
+// console.log(typeof(1+"2"));//string
+// console.log("1+2"); //1+2
 
-console.log("1"+"2"); //12
+// console.log("1"+"2"); //12
+// console.log(typeof("1"+"2")); //string
 
-console.log("1"+2+2);  //122
-console.log(1+2+"2"); //32
-console.log(3+4*5%3); //5
-console.log((3+4)*5%3); */
+
+// console.log("1"+2+2);  //122
+// In JavaScript, when you use the + operator with a string and a number, it performs string concatenation.
+// So, "1" is a string, and when you add "1" to 2,
+//  it converts 2 to a string and concatenates them together to form "12". 
+// Then, adding another 2 to "12" still concatenates as strings, resulting in "122"
+
+// console.log(1+2+"2"); //32
+// In JavaScript, when you use the + operator with a number and a string,
+//  it performs arithmetic addition if both operands are numbers. If either operand is a string, it concatenates them together as strings.
+// So, in the expression 1+2+"2", the addition 1+2 results in 3 (a number).
+//  Then, when you concatenate this number 3 with the string "2", it converts the number 3 to a string and 
+// //  concatenates them together, resulting in "32".
+
+// console.log(3+4*5%3); //5
+// console.log((3+4)*5%3);
   //note
-  /*
-        If string is in first then all treated as string 
-        if string is in last Conversion will happen before numbers
-  */
+  
+      //   If string is in first then all treated as string
+      //   if string is in last Conversion will happen before numbers
+ 
 
-   /*  console.log(+true);  //1
-    console.log(-false); //-0
-    console.log(+"");//0
-    console.log(-""); //-0 */
-    /* let num1,num2,num3
-    num1=num=2=num3 =2+2 */
+//     console.log(+true);  //1
+//     console.log(-false); //-0
+//     console.log(+"");//0
+//     console.log(-""); //-0 */
+//     let num1,num2,num3
+//     num1=num=2=num3 =2+2
 
 
-   /* let gameCounter = 100
-   gameCounter++;
-   console.log(gameCounter); */ //101
+//    let gameCounter = 100
+//    console.log(gameCounter);
+//    gameCounter++;
+//    console.log(gameCounter);  //101
 
-   let gameCounter = 100
-   ++gameCounter;
-   console.log(gameCounter);
-   console.log(gameCounter)
+let gameCounter = 100
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+console.log(gameCounter)
