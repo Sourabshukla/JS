49c40975fdc5c62a3a9ce62b53b1b8cc4ad1e7a7
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-26 21:57:26 +0530

    Update 02_Scopes.js

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -1,5 +1,5 @@
-//{}//scope
-//var c=300 //global scope
+//  {}    //block sccope
+//var c=300        //global scope
 
 let a=300
 
@@ -7,10 +7,10 @@ let a=300
 if(true){  //block scope
     let a=10;
     const b=20
-   //console.log("Inner :",a);
+   console.log("Inner :",a);   //Inner : 10 
 }
 
-//console.log(a);
+console.log(a); //300
 // console.log(b); //ReferenceError: b is not defined
 //console.log(c); //30
 
@@ -32,15 +32,17 @@ if(true){  //block scope
     const username="sourab"
     if(username==="sourab"){
         const website=" youtube"
-        //console.log(username + website);
+        console.log(username + website); //sourab youtube
     }
     //console.log(website);//ReferenceError: website is not defined
  }
 
 
-//console.log(username);
+// console.log(username);
 
 //+++++++++++++++++++++++ intresting ++++++++++++++++++
+
+
 console.log(addone(5)); // hoisting wherever we declared it take acc to below and all
 function addone(num){
     return num+1
@@ -51,3 +53,10 @@ const addTwo=function(num){
     return num+2
 }
 addTwo(5)
+// console.log(addTwo()); //TypeError: addTwo is not a function
+console.log(addTwo(8));
+
+// console.log(multiplyOne(4,6)) //TypeError: multiplyOne is not a function
+
+const multiplyOne = (x , y)=> x*y; //arrow function
+console.log(multiplyOne(4,6)); //24
