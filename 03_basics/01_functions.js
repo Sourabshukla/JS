5f863e4235ebfc1e05532ff1d097f983fd4996dc
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-26 21:45:45 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,3 +1,9 @@
+//syntax
+
+// function name(params) {
+    
+// }
+
 
 function sayMyName(){
     console.log("S");
@@ -10,11 +16,11 @@ function sayMyName(){
 
 }
 
-//console.log(sayMyName());
+// console.log(sayMyName());
 
 // function addTwoNumbers(number1,number2){  // parameter
 //     console.log(number1 + number2); 
-//     //NaN output if does not call
+//     //NaN output if does not call return statement
 // }
 // const result=addTwoNumbers(3,4) //arguments (when callong function)
 
@@ -27,31 +33,31 @@ function addTwoNumbers(number1,number2){  // parameter
 }
 const result=addTwoNumbers(3,4) //arguments (when callong function)
 
-//console.log("Result: ", result);
+// console.log("Result: ", result);
 
-// function loginUserMessage(username){
-//     return `${username} just logged in`
-// }
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
 
-// console.log(loginUserMessage("sourab"))//logged
+// console.log(loginUserMessage("sourab"))//sourab just logged in
 
 function loginUserMessage(username="sam"){  //by default value sam
     if(username===undefined){  // or (!username)
         console.log("please enter a username");
         return
     }
-    return `${username} just logged in`
+    return `${username} just logged in`   //sam just logged in 
 }
 
-//console.log(loginUserMessage())//undefined just logged in
+console.log(loginUserMessage())//undefined just logged in
 
-//... spread or rest operator  if we  gave only 1 parameter and in argument gave extra value so we use   ...  operator it means gather all
+//... spread or rest operator  if we gave only 1 parameter and in argument gave extra value so we use   ...  operator it means gather all
 //    ...   =>  rest operator
 
 function calsulateCartPrice(...num1){
     return num1
 }
-///console.log(calsulateCartPrice(5,25));
+// console.log(calsulateCartPrice(5,25)); //  [5,25]
 
 const user ={
 
@@ -62,16 +68,19 @@ const user ={
 function handleObject(anyobject){
     console.log(`username is ${anyobject.username } and price is ${anyobject.price}`);
 }
-//handleObject(user)
+handleObject(user)
 
 handleObject({
     username:"sam",
     price:399 
 })
 
+
+
+
 const myNewArray =[200,400,100,600]
 
 function returnSecondsValue(getArray){
     return getArray[1]
 }
-console.log( returnSecondsValue([200,400,500,1000]));
+console.log( returnSecondsValue(myNewArray)); //400
